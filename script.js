feb6407ade1bb50223981f884352fb40686e588b
ROHITESH-KUMAR-JAIN <thekumarjain@gmail.com>
2026-03-11 23:12:09 +0530

    .

---
+++ script.js
@@ -0,0 +1,26 @@
+const sections=document.querySelectorAll('.space,.statement')
+
+const observer=new IntersectionObserver(entries=>{
+
+entries.forEach(entry=>{
+
+if(entry.isIntersecting){
+
+entry.target.style.opacity=1
+entry.target.style.transform="translateY(0)"
+
+}
+
+})
+
+})
+
+sections.forEach(s=>{
+
+s.style.opacity=0
+s.style.transform="translateY(50px)"
+s.style.transition="all 1s ease"
+
+observer.observe(s)
+
+})
